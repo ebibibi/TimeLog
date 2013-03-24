abadb738bd396e9e3952e901f2bb912a3e7f234c
Masahiko Ebisuda <ebibibi@gmail.com>
2013-03-24 21:46:24 +0900

    created job class. add name property.

---
+++ TimeLogTest/JobTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TimeLog;
+
+namespace TimeLogTest
+{
+    [TestClass]
+    public class JobTests
+    {
+        [TestMethod]
+        public void JobName()
+        {
+            //Setup
+            string jobname = "ジョブ名";
+
+            Job job = new Job();
+
+            //Execute
+            job.name = jobname;
+
+            //Test
+            Assert.AreEqual<string>(jobname, job.name);
+            
+        }
+    }
+}
