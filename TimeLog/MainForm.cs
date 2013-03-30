87eb034053e764ad8d51a96bd7678bfe6c8efded
Masahiko Ebisuda <ebibibi@gmail.com>
2013-03-30 16:02:25 +0900

    added create job icon function.

--- TimeLog/MainForm.cs
+++ TimeLog/MainForm.cs
@@ -12,9 +12,47 @@ namespace TimeLog
 {
     public partial class MainForm : Form
     {
+        private static JobManager jobManager = new JobManager();
+
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void jobNameTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void jobNameTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == System.Windows.Forms.Keys.Enter)
+            {
+                if (jobNameTextBox.Text != null)
+                {
+                    string jobname = jobNameTextBox.Text;
+                    if (jobManager.IsThereJob(jobname) == false)
+                    {
+                        jobManager.CreateNewJob(jobname);
+                        this.CreateJobIcon(jobname);
+                    }
+                    jobNameTextBox.Text = "";
+                }
+            }
+
+        }
+
+        private void CreateJobIcon(string jobname)
+        {
+            Button jobIcon = new Button();
+            jobIcon.Text = jobname;
+
+            jobIconsFlowLayoutPanel.Controls.Add(jobIcon);
         }
     }
 }
