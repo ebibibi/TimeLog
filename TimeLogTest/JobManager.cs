ba24f2ce488dbd0dd93f2ab0116769b4b6b77d54
Masahiko Ebisuda <ebibibi@gmail.com>
2013-03-30 11:27:45 +0900

    added JobManager Class.

---
+++ TimeLogTest/JobManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TimeLog
+{
+    class JobManager
+    {
+        internal void CreateNewJob(string p)
+        {
+            return;
+        }
+
+        internal bool IsThereJob(string p)
+        {
+            return true;
+        }
+    }
+}
---
+++ TimeLogTest/JobManagerTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TimeLog;
+
+namespace TimeLogTest
+{
+    [TestClass]
+    public class JobManagerTests
+    {
+        [TestMethod]
+        public void CreateJob()
+        {
+            //Setup
+            JobManager manager = new JobManager();
+
+            //Execute
+            manager.CreateNewJob("test job1");
+            
+            //Test
+            Assert.IsTrue(manager.IsThereJob("test job1"));
+
+        }
+    }
+}
